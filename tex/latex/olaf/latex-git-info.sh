08037b21aaef7dd44c51fab5c311fe162c62b578
Olaf Merkert <olaf@m-merkert.de>
2012-04-10 14:47:12 +0200

    ein Skript, um Versionsinformation aus git in LaTeX Dokumente einzufuegen

---
+++ tex/latex/olaf/latex-git-info.sh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+
+# determine the branch name
+branch=`git branch | egrep '^\*'`
+branch=${branch/* /}
+
+# determine the current commit
+commit=`git log -1 --format=%t`
+
+# determine whether there are pending changes
+clean=`git status | grep "working directory clean"`
+
+# determine when the last commit occured
+date=`git log -1 --format="%ci"`
+
+
+# output for LaTeX:
+print "\\\noindent {"
+# print "{\\\textit{Version information:} \\\\\\"
+print "{\\\textsf{branch:}  \\\verb|$branch|} \\\quad"
+print "{\\\textsf{commit:}  \\\verb|$commit|} \\\quad"
+print "{\\\textsf{commit date:}    \\\textrm{$date}}"
+
+if [[ -z "$clean" ]]; then
+    print "\\\hfill \\\textbf{*}"
+fi
+print "}"
